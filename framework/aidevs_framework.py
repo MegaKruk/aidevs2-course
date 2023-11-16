0bda5d1f29a5107084d78178519ab65da5c1adea
MegaKruk <megakruk7@gmail.com>
2023-11-16 20:01:48 +0100

    extended aidevs_framework and finished task search

--- framework/aidevs_framework.py
+++ framework/aidevs_framework.py
@@ -1,5 +1,7 @@
 import requests
-from secrets.secrets import my_api_key
+import json
+import uuid
+from secrets.secrets import my_api_key, ADA_002_API_URL, OPENAI_API_KEY
 
 
 headers = {"Content-Type": "application/json"}
@@ -45,3 +47,60 @@ def solve_task(answer, token):
     data = response.json()
     print(f"Response from answer: \n{data}")
     return data
+
+def read_json_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+def create_collection_in_qdrant(collection_name, documents, QDRANT_URL):
+    # Define the schema of the collection, including the vectors field
+    collection_schema = {
+        "name": collection_name,
+        # "vector_size": 1536,  # Set the appropriate vector size
+        # "distance": "Cosine",  # Or another distance metric
+        "vectors": {
+            "size": 1536,
+            "distance": "Cosine"
+        }
+    }
+
+    # Create collection
+    response = requests.get(f"{QDRANT_URL}/collections/{collection_name}")
+    if response.status_code == 404:
+        print("Collection does not exist, create it")
+        response = requests.put(f"{QDRANT_URL}/collections/{collection_name}", json=collection_schema)
+        if response.status_code != 200:
+            raise Exception(f"Error creating collection: {response.json()}")
+    else:
+        print("Collection exists")
+    # Insert documents into the collection
+    len_documents = len(documents)
+    for idx, doc in enumerate(documents):
+        HEADERS = {
+            "Authorization": f"Bearer {OPENAI_API_KEY}",
+            "Content-Type": "application/json"
+        }
+        payload = {
+            "model": "text-embedding-ada-002",
+            "input": f"{doc['info']}"
+        }
+        response = requests.post(ADA_002_API_URL, headers=HEADERS, json=payload)
+        vector = [0] * 1536
+        if response.status_code == 200:
+            vector = response.json().get("data", [{}])[0].get("embedding")
+        else:
+            # Handle error
+            response.raise_for_status()
+        doc_id = str(uuid.uuid4())
+        payload = {
+            "points": [{
+                "id": doc_id,  # Using URL as a unique identifier
+                "vector": vector,  # The vector obtained from ADA
+                "payload": doc
+            }]
+        }
+        response = requests.put(f"{QDRANT_URL}/collections/{collection_name}/points", json=payload)
+        if response.status_code != 200:
+            raise Exception(f"Error inserting document: {response.json()}")
+        print(f"{idx+1} / {len_documents}")
+    print(f"Collection '{collection_name}' created and documents inserted.")
---
+++ search.py
@@ -0,0 +1,59 @@
+from framework.aidevs_framework import *
+from secrets.secrets import GPT35TURBO_API_URL, OPENAI_API_KEY, QDRANT_URL
+
+
+task_name = "search"
+question = None
+
+token = get_token(task_name=task_name)
+task = authenticate(token=token, question=question)
+
+####################################################################
+
+QUERY = task["question"]
+COLLECTION_NAME = "task_search_colection"
+# JSON_FILE_PATH = "./archiwum.json"
+#
+# documents = read_json_file(JSON_FILE_PATH)
+# create_collection_in_qdrant(COLLECTION_NAME, documents, QDRANT_URL)
+
+headers = {
+    "Authorization": f"Bearer {OPENAI_API_KEY}",
+    "Content-Type": "application/json"
+}
+embedding_payload = {
+    "model": "text-embedding-ada-002",
+    "input": QUERY
+}
+embedding_response = requests.post(ADA_002_API_URL, headers=headers, json=embedding_payload)
+if embedding_response.status_code != 200:
+    raise Exception(f"Error getting embedding: {embedding_response.json()}")
+
+query_embedding = embedding_response.json().get("data", [{}])[0].get("embedding")
+search_payload = {
+    "vector": query_embedding,
+    "limit": 1,
+    "with_payload": True,
+    "with_vector": True
+    # "filter": {
+    #     "must": [
+    #         {
+    #             "key": 'source',
+    #             "match": {
+    #                 "value": COLLECTION_NAME
+    #             }
+    #         }
+    #     ]
+    # }
+}
+search_response = requests.post(f"{QDRANT_URL}/collections/{COLLECTION_NAME}/points/search", json=search_payload)
+if search_response.status_code != 200:
+    raise Exception(f"Error searching in Qdrant: {search_response.json()}")
+
+search_results = search_response.json()
+
+answer = search_results[("result")][0]["payload"]["url"]
+print(answer)
+####################################################################
+
+solve_task(answer=answer, token=token)
